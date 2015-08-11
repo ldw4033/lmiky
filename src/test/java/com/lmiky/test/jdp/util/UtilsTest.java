d9d50a0cb826a63bbd79df6be8b0e5b7b6d678f5
linminqin <flylmiky@gmail.com>
2015-08-11 21:39:08 +0800

    commit

--- src/test/java/com/lmiky/test/jdp/util/UtilsTest.java
+++ src/test/java/com/lmiky/test/jdp/util/UtilsTest.java
@@ -1,7 +1,16 @@
 package com.lmiky.test.jdp.util;
 
-import org.junit.Test;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.lmiky.jdp.util.PropertiesUtils;
 import com.lmiky.jdp.util.StringUtils;
 import com.lmiky.jdp.util.UUIDGenerator;
 import com.lmiky.test.BaseTest;
@@ -12,6 +21,9 @@ import com.lmiky.test.BaseTest;
  * @date 2013-10-16
  */
 public class UtilsTest extends BaseTest {
+    
+    @Autowired
+    private VelocityEngine velocityEngine;
 	
 	@Test
 	public void generate() {
@@ -19,6 +31,31 @@ public class UtilsTest extends BaseTest {
 			System.out.println(UUIDGenerator.generateString());
 		}
 	}
+	
+	@Test
+    public void testVelocityEngine() {
+           System.out.println(velocityEngine);
+           //String template = PropertiesUtils.getStringContextValue("system.test");
+           String template = "我的<a href=\"http://localhost?userId=${userId}\">收藏</a>";
+           System.out.println(template);
+           StringWriter writer = null;  
+           Map<String, String> dataMap = new HashMap<>();
+           dataMap.put("userId", 1 + "");
+           try {  
+               writer = new StringWriter();  
+               VelocityContext context = new VelocityContext(dataMap);  
+               velocityEngine.evaluate(context, writer, "", template);  
+               System.out.println(writer.toString());
+           } finally {  
+               if (writer != null) {  
+                   try {  
+                       writer.close();  
+                   } catch (IOException e) {  
+                       e.printStackTrace();
+                   }  
+               }  
+           }  
+    }
 	
 	@Test
 	public void getFirstLetterTest() {
