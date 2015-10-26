867ef8c93cba321f0c57ec6ba01c601abbe2cd28
linminqin <flylmiky@gmail.com>
2015-10-26 17:35:47 +0800

    修改http工具类

---
+++ platform/platform-common/src/main/java/com/lmiky/platform/http/HttpsOperator.java
@@ -0,0 +1,61 @@
+package com.lmiky.platform.http;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+/**
+ * HTTPS请求工具
+ * @author lmiky
+ * @date 2015年10月26日 下午1:41:04
+ */
+public class HttpsOperator extends HttpOperator {
+	protected static HttpsOperator httpsInstance;
+
+	/**
+	 * 说明
+	 * @return
+	 * @author lmiky
+	 * @date 2015年10月26日 下午1:57:45
+	 */
+	public static synchronized HttpsOperator getInstance() {
+		if (httpsInstance == null) {
+			httpsInstance = new HttpsOperator();
+		}
+		return httpsInstance;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.lmiky.platform.http.HttpOperator#createDefaultClient()
+	 */
+	public CloseableHttpClient createDefaultClient() throws Exception {
+		HttpClientBuilder httpClientBuilder = createHttpClientBuilder();
+		setSSL(httpClientBuilder);
+		return httpClientBuilder.build();
+	}
+	
+	/**
+	 * 设置SSL
+	 * @param httpClientBuilder
+	 * @throws Exception
+	 * @author lmiky
+	 * @date 2015年10月26日 下午2:42:55
+	 */
+	public void setSSL(HttpClientBuilder httpClientBuilder) throws Exception {
+		SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+			// 信任所有
+			public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				return true;
+			}
+		}).build();
+		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
+		httpClientBuilder.setSSLSocketFactory(sslsf);
+	}
+}
---
+++ platform/platform-common/src/main/java/com/lmiky/platform/http/client/AutoHttpClient.java
@@ -0,0 +1,84 @@
+package com.lmiky.platform.http.client;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+
+/**
+ * 自动关闭HTTP客户端
+ * @author lmiky
+ * @date 2015年10月26日 下午3:34:57
+ */
+@SuppressWarnings("deprecation")
+public class AutoHttpClient extends CloseableHttpClient {
+	private CloseableHttpClient target;
+	private boolean autoClose = true;	//是否自动关闭
+	
+	public AutoHttpClient() {
+		
+	}
+
+	public AutoHttpClient(CloseableHttpClient target,  boolean autoClose) {
+		this.target = target;
+		this.autoClose = autoClose;
+	}
+
+	/**
+	 * @return the target
+	 */
+	public CloseableHttpClient getTarget() {
+		return target;
+	}
+
+	/**
+	 * @param target the target to set
+	 */
+	public void setTarget(CloseableHttpClient target) {
+		this.target = target;
+	}
+	
+	/**
+	 * @return the autoClose
+	 */
+	public boolean isAutoClose() {
+		return autoClose;
+	}
+
+	/**
+	 * @param autoClose the autoClose to set
+	 */
+	public void setAutoClose(boolean autoClose) {
+		this.autoClose = autoClose;
+	}
+
+	@Override
+	public HttpParams getParams() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientConnectionManager getConnectionManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected CloseableHttpResponse doExecute(HttpHost target, HttpRequest request, HttpContext context) throws IOException, ClientProtocolException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
